955f7e209d37502d0796a9de862707e4c9d4730f
Eldarian <eldarian3@gmail.com>
2019-03-28 19:06:55 +0300

    Server
    Added data.json with simple database, server.js with simple node.js server. Now application can download old tasks.

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 $(function() {
     
     'use strict';
-    var taskNum = 0;
-    function addTask() {
-       /* Добавляет пустую задачу в конец списка */
-       taskNum++;
-        let curTime = curTimes();
-        let taskBox = '<li class="task" id="task-0'+ taskNum +'">'+
-            '<input class="isDone" type="checkbox">'+
-            '<input class="taskName" type="text" placeholder="Task Name" value="Task '+ taskNum +'">'+
-            '<textarea class="taskDescription" placeholder="Task Description"></textarea>'+
-            '<select class="taskPriority">'+
-                '<option>High Priority</option>'+
-                '<option>Medium Priority</option>'+
-                '<option>Low Priority</option>'+
-            '</select> '+
-            '<p class="date-time">'+ curTime +'</p>'+
-            '<button class="save-button">Save</button>'+
-            '<button class="delete-button">Delete</button>'+
-    '</li>';
-       $('.task-list').append(taskBox);
-    // Задача добавлена
+    
+
+
+    var taskId = 0; //Счётчик задач (будет получаться от сервера)
+    getTasks(); //Сразу проверяем базу данных на наличие задач.
+
+    function loadTask(task) {
+        /* Добавляет задачу в конец списка. */
+        console.log('Start loading');
+        console.log(task.name);
+        let taskBox = '<li class="task" id="task-0'+ taskId +'">'+
+        '<input class="isDone" type="checkbox" ';
+        if(task.isDone) {
+            taskBox +='checked';
+        }
+             taskBox+='><input class="taskName" type="text" placeholder="Task Name" value="'+ task.name +'">'+
+             '<textarea class="taskDescription" placeholder="Task Description" value="'+ task.description +'"></textarea>'+
+             '<select class="taskPriority">'+'<option';
+
+            if(task.priority==1) {
+                taskBox += 'selected';
+            } 
+            taskBox += '>High Priority</option>'+
+            '<option';
+
+            if(task.priority==2) {
+                taskBox += 'selected';
+            } 
+            taskBox +='>Medium Priority</option>'+
+            '<option';
+
+            if(task.priority==3) {
+                taskBox += 'selected';
+            } 
+
+            taskBox +='>Low Priority</option>'+
+             '</select> '+
+             '<p class="date-time">'+ task.creationDate +'</p>'+
+             '<button class="save-button">Save</button>'+
+             '<button class="delete-button">Delete</button>'+
+     '</li>';
+     $('.task-list').append(taskBox);
+     console.log('task has been loaded');
+     // Задача добавлена
     }
 
     function curTimes() {
@@ -69,8 +93,34 @@ $(function() {
         $(id).remove();
     }
 
-    function editTask(task, parameter) {
-        /* Заменяет значения в формах по умолчанию value */
+    function getTasks() {
+        //Получает задачи с сервера и добавляет их на страницу
+        console.log('start getting tasks');
+        var xhr = new XMLHttpRequest(); //Связываемся с сервером
+        xhr.open('GET', '/data.json', true);
+        xhr.send();
+        xhr.onreadystatechange = function() { // (3)
+            if (xhr.readyState != 4) return;
+           
+        var tasks = [];
+        if (xhr.status != 200) {
+            alert(xhr.status + ': ' + xhr.statusText);
+        } else {
+            let tasksMessage = JSON.parse(xhr.responseText);
+            taskId = tasksMessage.taskId;
+            tasks = tasksMessage.tasks;
+            console.log(tasks);
+
+         for(var i=0; i<tasks.length; i++) {
+             loadTask(tasks[i]);
+         }
+        }
+    }
+    }   
+
+
+    function saveTask(task, parameter) {
+        /* Отправляет задачу на сервер*/
     }
 
     function sortTasks(fromNew) {
@@ -87,4 +137,27 @@ $(function() {
         let id = '#' + $(this).parent().attr('id');
         removeTask(id);
     }); //Нажатие кнопки Delete удаляет текущую задачу*/
+
+    function addTask() {
+        /* Добавляет задачу в конец списка. */
+        console.log(taskId);
+        taskId++;
+        let curTime = curTimes(); //Текущее время
+        let taskBox;
+         taskBox = '<li class="task" id="task-0'+ taskId +'">'+
+             '<input class="isDone" type="checkbox">'+
+             '<input class="taskName" type="text" placeholder="Task Name" value="Task '+ taskId +'">'+
+             '<textarea class="taskDescription" placeholder="Task Description"></textarea>'+
+             '<select class="taskPriority">'+
+                 '<option>High Priority</option>'+
+                 '<option>Medium Priority</option>'+
+                 '<option>Low Priority</option>'+
+             '</select> '+
+             '<p class="date-time">'+ curTime +'</p>'+
+             '<button class="save-button">Save</button>'+
+             '<button class="delete-button">Delete</button>'+
+     '</li>';
+     $('.task-list').append(taskBox);
+     // Задача добавлена
+     }
 });
